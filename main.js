8b6093c373368432e85fea48829577345c2ec147
Aleksey <boston8119@mail.ru>
2024-07-30 15:22:16 +0300

    loc

--- main.js
+++ main.js
@@ -12,70 +12,84 @@ import {
 let numbers = [];
 let operations = [];
 
-document.addEventListener('DOMContentLoaded', getLocalItem)
+try {
 
-function getLocalItem() {
-    const numberREC = localStorage.getItem('numberLocal');
-    RESULT.value = numberREC;
-};
+    document.addEventListener('DOMContentLoaded', getLocalItem)
 
-CALC_ElELEMENT.forEach(item => {
-    item.addEventListener('click', parseAndCalculate)    
-});
+    function getLocalItem() {
+        const numberREC = localStorage.getItem('numberLocal');
+        RESULT.value = numberREC;
+    }; 
 
-function parseAndCalculate(e) {
-    // Массивы для хранения цифр и операций
-    
-    
-    let expression = e.target.value;
-    // console.log(expression);
-    // Разбиваем выражение на части
-    let parts = expression.split(/(\d+|\+|-|\*|\/)/) /*.forEach( e => e != "")*/;
-    console.log(parts);
+    CALC_ElELEMENT.forEach(item => {
+        item.addEventListener('click', parseAndCalculate)    
+    });
 
-    RESULT.value += expression
 
-    // Проходим по разбитым частям
-    for (let i = 0; i < parts.length; i++) {
-        if (!isNaN(parts[i])) { // Если часть - число
-            if(parts[i] !== "") {
-            // console.log(parts[i]);
-            numbers.push(parseFloat(parts[i]));
+
+    function parseAndCalculate(e) {
+        // Массивы для хранения цифр и операций
+        
+        
+        let expression = e.target.value;
+        console.log(typeof expression);
+        // Разбиваем выражение на части
+        let parts = expression.split(/(\d+|\+|-|\*|\/)/) /*.forEach( e => e != "")*/;
+        console.log(parts);
+
+        RESULT.value += expression
+
+        // Проходим по разбитым частям
+        for (let i = 0; i < parts.length; i++) {
+            if (!isNaN(parts[i])) { // Если часть - число
+                if(parts[i] !== "") {
+                // console.log(parts[i]);
+                numbers.push(parseFloat(parts[i]));
+                
+                }
+            } else if (parts[i] === '+' || parts[i] === '-' || parts[i] === '*' || parts[i] === '/') {
+                operations.push(parts[i]);
             }
-        } else if (parts[i] === '+' || parts[i] === '-' || parts[i] === '*' || parts[i] === '/') {
-            operations.push(parts[i]);
         }
+
+
+        // Выполняем вычисления
+        let result = numbers[0];
+        for (let i = 0; i < operations.length; i++) {
+            switch (operations[i]) {
+                case '+':
+                    result += numbers[i + 1];
+                    break;
+                case '-':
+                    result -= numbers[i + 1];
+                    break;
+                case '*':
+                    result *= numbers[i + 1];
+                    break;
+                case '/':
+                    result /= numbers[i + 1];
+                    break;
+            }
+        }
+        console.log(result);
+        localStorage.setItem("numberLocal", result);
+        // numbers.push(result);
+
+        console.log(numbers);
+        console.log(operations);
+
+        CALC_RESULT.addEventListener("click", () => RESULT.value = result );
     }
 
+    CALC_DEL_ALL.addEventListener("click", () => {
+        numbers.splice(0,numbers.length);
+        operations.splice(0,operations.length);
+        RESULT.value = "";
+        
+        HISTORY.value = numbers
+    })
 
-    // Выполняем вычисления
-    let result = numbers[0];
-    for (let i = 0; i < operations.length; i++) {
-        switch (operations[i]) {
-            case '+':
-                result += numbers[i + 1];
-                break;
-            case '-':
-                result -= numbers[i + 1];
-                break;
-            case '*':
-                result *= numbers[i + 1];
-                break;
-            case '/':
-                result /= numbers[i + 1];
-                break;
-        }
-    }
-    localStorage.setItem("numberLocal", result);
-    console.log(numbers);
-    console.log(operations);
-
-    CALC_RESULT.addEventListener("click", () => RESULT.value = result)
-    
+} catch (e){
+    alert("код гавно")
+    alert(e)
 }
-
-CALC_DEL_ALL.addEventListener("click", () => {
-    numbers.splice(0,numbers.length);
-    operations.splice(0,operations.length);
-    RESULT.value = "";
-})
